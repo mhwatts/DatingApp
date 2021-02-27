d419b7f4fbfa124efdd5f8321200dd81d6e62f31
Michael Watts <michael.watts1@verizon.net>
2021-02-27 09:17:40 -0500

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics.CodeAnalysis;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+
+
+        #region Unused Code
+        /*        
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        } 
+        */
+        #endregion
+
+    }
+}
